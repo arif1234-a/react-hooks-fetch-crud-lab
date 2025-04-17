6ee7eeff0473e6aadcdf1897ec0e54aaa6ea2a78
arifrahim <arifrahimjamal@gmail.com>
2025-04-17 17:27:17 +0300

    completed the rest of the fetching code

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((res) => res.json())
+      .then((data) => setQuestions(data))
+      .catch((error) => console.error("Error fetching questions:", error));
+  }, []);
+
+  function handleUpdateQuestion(updatedQuestion) {
+    setQuestions(
+      questions.map((q) => (q.id === updatedQuestion.id ? updatedQuestion : q))
+    ); // Update state
+  }
+
+  function handleDeleteQuestion(deletedId) {
+    setQuestions(questions.filter((q) => q.id !== deletedId)); // Remove from state
+  }
+
   return (
-    <section>
-      <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
-    </section>
+    <ul>
+      {questions.map((question) => (
+        <QuestionItem
+          key={question.id}
+          question={question}
+          onUpdateQuestion={handleUpdateQuestion}
+          onDeleteQuestion={handleDeleteQuestion}
+        />
+      ))}
+    </ul>
   );
 }
 
